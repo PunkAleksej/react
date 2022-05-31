5dd022432564390c3d24d0c62df7108f33eea1d8
Alex Dudnikov <alekdudnikov@mail.ru>
2022-05-31 17:38:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
@@ -17,8 +18,42 @@ function App() {
         >
           Learn React
         </a>
+
+        <div class='game-map'>
+          <div className="board-row">
+            0
+          </div>
+          <div className="board-row">
+            1
+          </div>
+          <div className="board-row">
+            2
+          </div>
+          <div className="board-row">
+            3
+          </div>
+          <div className="board-row">
+            4
+          </div>
+          <div className="board-row">
+            5
+          </div>
+          <div className="board-row">
+            6
+          </div>
+          <div className="board-row">
+            7
+          </div>
+          <div className="board-row">
+            8
+          </div>
+      </div>
       </header>
+
     </div>
+    
+
+
   );
 }
 
